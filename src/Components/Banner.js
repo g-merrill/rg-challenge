7e34566a6e239d276d0da280be060d6ef9fb5dd5
Greg <gmerrill03@gmail.com>
2020-01-27 11:29:49 -0800

    Add Banner styling

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,8 +2,22 @@ import React from 'react';
 import './scss/Banner.scss';
 
 const Banner = () => (
-  <div className="Banner h100">
-    Banner
+  <div className="Banner">
+    <img
+      className='rg-logo-white'
+      src="/images/logo-white.svg"
+      alt="rooster grin"
+    />
+    <div className="banner-content">
+      <h1 className='welcome-txt'>WELCOME TO</h1>
+      <h2 className='rg-text'>Rooster Grin</h2>
+      <button className='banner-btn'>BUTTON BUTTON</button>
+    </div>
+    <img
+      className='Banner-img'
+      src='/images/hero.jpg'
+      alt="bridge"
+    />
   </div>
 );
 
