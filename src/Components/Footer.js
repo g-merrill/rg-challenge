d25b6f78d439d4eb5c29a2213384c80f78aff225
Greg <gmerrill03@gmail.com>
2020-01-27 20:32:22 -0800

    Add Footer styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,20 @@ import React from 'react';
 import './scss/Footer.scss';
 
 const Footer = () => (
-  <div className="Footer h100">
-    Footer
+  <div className="Footer">
+    <p className='Footer-txt'>Follow Us:</p>
+    <div className="Footer-logo-ctnr">
+      <img
+        className='Footer-ig-svg'
+        src="/images/instagram.svg"
+        alt="instagram"
+      />
+      <img
+        className='Footer-fb-svg'
+        src="/images/facebook.svg"
+        alt="facebook"
+      />
+    </div>
   </div>
 );
 
